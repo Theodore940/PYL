4cfdd5f4bf539463504ed550a110cf5401720ee1
Ryan L <Ryan L>
2016-09-24 12:57:18 -0500

    variables and constructor in data struct class

--- PressYourLuck/PressYourLuck/DataStructureClass.cs
+++ PressYourLuck/PressYourLuck/DataStructureClass.cs
@@ -7,6 +7,36 @@ namespace PressYourLuck
 {
     class DataStructureClass
     {
+        private struct player
+        {
+            public int score, spins, passedSpins;
+            public string ans; //place to store a players answer (might not be needed)
+        }
 
+        private int numPlayers;
+        private List<string> questions; 
+        private List<string> answers;
+        private player[] playerData;
+
+        public DataStructureClass(int players)
+        {
+            string line;
+            //load question and answers from file
+            System.IO.StreamReader fileQA = new System.IO.StreamReader("Questions.txt");
+
+
+            //TODO: make sure this doesn't break because a Q doesn't have an A
+            while ((line = fileQA.ReadLine()) != null)
+            {
+                questions.Add(line);
+                line = fileQA.ReadLine();
+                answers.Add(line);
+
+            }
+
+            // TODO: initialize playerData 
+
+
+        }
     }
 }
